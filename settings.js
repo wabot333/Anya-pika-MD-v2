db65637a2fbb2902a93935f2b390623a6b968e51
wabot333 <127656844+wabot333@users.noreply.github.com>
2023-03-12 16:44:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,31 +3,31 @@ const chalk = require('chalk')
 
 
 global.botname = "Queen Anya" 
-global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
+global.botLanguage = "English" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
+global.author = "MAhikalpa"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
-global.DocumentTitle = "MASTER⚔️PIKA" 
+global.waterMark = "×͜× King ✘ MAHI™ ✓" // Your own watermark 
+global.headerText = "Anya by Mahikalpa" // text for header of menu         
+global.DocumentTitle = "MASTER⚔️MAHI" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ownername = "mahikalpa"   // Owner name //        
+global.ownernumber = ['94769735936'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_Mahikalpa" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
+global.region = "Sri lanka" // your country name
 global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
-global.email = "example@exmaple.com" // put your email here
+global.district = "Kurunegala" // your district name
+global.timezone = "Asia/Colombo" // search on google if you don't know the timezone of your country //
+global.instagramId = "3.69_Mahikalpa" // Your insta ID
+global.email = "mahikalpa123@exmaple.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
 global.premium = ['916900******','917355******']  // available soon
-global.sudo = ['916900******','917355******']  // Co - Owners //
+global.sudo = ['94769735936','917355******']  // Co - Owners //
 
 global.prefa = ['-']
 
